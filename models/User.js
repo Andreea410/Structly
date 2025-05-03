541f35ff42b7afc082c38bf74e7496457fc7ea1f
Andreea410 <andreeaungur494@yahoo.com>
2025-05-03 19:12:49 +0300

    Refactor: Changed the User structure

--- models/User.js
+++ models/User.js
@@ -2,9 +2,10 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  passwordHash: { type: String, required: true },
-  role: { type: String, enum: ["user", "admin"], default: "user" },
+  email: { type: String, required: true, unique: true },
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  role: { type: String, enum: ['user', 'admin'], default: 'user' }
 });
 
 export default mongoose.models.User || mongoose.model("User", userSchema);
