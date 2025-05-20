558102f75dcf9c0de816616c8363323455a25b15
Andreea410 <andreeaungur494@yahoo.com>
2025-05-20 19:24:43 +0300

    Fix: Fixed again some bad imports

--- app/api/monitored-users/route.js
+++ app/api/monitored-users/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 import {dbConnect} from '../../../lib/dbConnect';
-import MonitoredUser from '../../../models/monitoredUser';
+import MonitoredUser from '../../../models/MonitoredUser';
 import { getCurrentUser } from '../../../lib/auth';
 
 export async function GET(req) {
