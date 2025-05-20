bccbc08101749cdeeac2e6be563d05674d83e0c3
Andreea410 <andreeaungur494@yahoo.com>
2025-05-20 19:27:55 +0300

    Another fix for some bad imports

--- app/api/socket/route.js
+++ app/api/socket/route.js
@@ -1,5 +1,5 @@
 import { Server } from "socket.io";
-import { setIO } from "../../lib/websocketInstance.js";
+import websocketInstance from "../../lib/websocketInstance";
 
 export default function handler(req, res) {
   if (!res.socket.server.io) {
@@ -8,15 +8,22 @@ export default function handler(req, res) {
       path: "/api/socket", 
     });
 
-    setIO(io); 
-
     res.socket.server.io = io;
 
     io.on("connection", (socket) => {
       console.log("Client connected");
+      
+      // Add the connection to our instance
+      const userId = socket.handshake.query.userId;
+      if (userId) {
+        websocketInstance.addConnection(userId, socket);
+      }
 
       socket.on("disconnect", () => {
         console.log("Client disconnected");
+        if (userId) {
+          websocketInstance.removeConnection(userId);
+        }
       });
     });
   }
